eec0bdd012f43a7b1dbe7b5414c6510a70fc6253
kimprojects <kimpita9@gmail.com>
2017-10-09 16:39:54 +0900

    Projects Section initial commit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,7 +10,7 @@
         <link rel="manifest" href="{{ asset('/site.webmanifest') }}">
         <link rel="apple-touch-icon" href="{{ asset('/icon.png') }}">
         <!-- Place favicon.ico in the root directory -->
-        
+
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
         <link rel="stylesheet" href="{{ asset('/css/normalize.css') }}">
@@ -22,6 +22,65 @@
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
         <![endif]-->
         <!--page content starts here-->
+        <header>
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="pds-logo" style="background-color:#fff;">
+                  <img class="img-responsive" src="{{ asset('/img/logo.png') }}" alt="WebDesignersCenter.com">
+                </div>
+              </div>
+              <div class="col-md-2 provider-btn hidden-xs">
+                <a class="btn btn-primary" href="#">Become a service provider</a>
+              </div>
+              <div class="col-md-4 pds-email">
+                <p>info@webdesignercenter.com</p>
+              </div>
+            </div>
+          </div>
+        </header>
+        <nav class="navbar navbar-inverse pds-menu-bar">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+          </div>
+          <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#">Projects</a></li>
+              <li><a href="#">How it works</a></li>
+              <li><a href="#">About us</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+              <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            </ul>
+          </div>
+        </div>
+        </nav>
+        <div class="slider">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-8 col-sm-offset-2">
+                <h1>Power to choose!</h1>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-6 col-sm-offset-3">
+                <p>Have companies bid for your project and select the best!</p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-5 project-btn pull-right">
+                <a class="btn btn-primary" href="#">Post a new project</a>
+              </div>
+            </div>
+         </div>
+        </div>
         @yield('content')
         <!--Page content ends here-->
         <script src="{{ asset('/js/vendor/modernizr-3.5.0.min.js') }}"></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,46 +1,8 @@
 @extends('layouts.main')
 
 @section('content')
-  <header>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-6">
-          <div class="pds-logo" style="background-color:#fff;">
-            <img class="img-responsive" src="{{ asset('/img/logo.png') }}" alt="WebDesignersCenter.com">
-          </div>
-        </div>
-        <div class="col-md-2 provider-btn hidden-xs">
-          <a class="btn btn-primary" href="#">Become a service provider</a>
-        </div>
-        <div class="col-md-4 pds-email">
-          <p>info@webdesignercenter.com</p>
-        </div>
-      </div>
-    </div>
-  </header>
-  <nav class="navbar navbar-inverse pds-menu-bar">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Home</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">How it works</a></li>
-        <li><a href="#">About us</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-      </ul>
-    </div>
-  </div>
-  </nav>
-  <div class="slider">
-  </div>
+  <section class="main-body">
+    <h2>Latest projects</h2>
+    <div class="strip"></div>
+  </section>
 @endsection
