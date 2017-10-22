df07570b7bee00c24abc0b8cc9de17f815c4e82b
Peter Kimani <kimpita9@gmail.com>
2017-10-22 17:39:29 +0900

    Site title addition initial commit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -29,10 +29,13 @@
         <header>
           <div class="container">
             <div class="row">
-              <div class="col-md-6">
+              <div class="col-md-1">
                 <div class="pds-logo" style="background-color:#fff;">
                   <img class="img-responsive" src="{{ asset('/img/logo.png') }}" alt="WebDesignersCenter.com">
                 </div>
+              </div>
+              <div class="col-md-5">
+                <h1>Web Designers Center</h1>
               </div>
               <div class="col-md-2 provider-btn hidden-xs">
                 <a class="btn btn-primary" href="/service-provider-sign-up">Become a service provider</a>
